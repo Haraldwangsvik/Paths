b881195bb76090ce5897c95b9f9295a5ea843368
Harald <haraldwf@stud.ntnu.no>
2023-02-06 12:51:05 +0100

    javadoc for the attributes

--- src/main/java/no/ntnu/idatx2001/Link.java
+++ src/main/java/no/ntnu/idatx2001/Link.java
@@ -2,11 +2,17 @@ package no.ntnu.idatx2001;
 
 public class Link {
 
+
+  // A describing text that indicated a choice or an action in a story.
+  // The text is the part of Link which is visible for the player.
   private String text;
 
+  // A string that identifies a passage (a part of a story).
   private String reference;
 
+  // A list with special objects that makes it possible to affect the features of a player.
   private List<Action> actions;
-  
+
+
 
 }
