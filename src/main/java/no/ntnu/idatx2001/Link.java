4d64b72cc7cd5f2df2c80f531ed2bb53b449b9a2
Harald <haraldwf@stud.ntnu.no>
2023-02-06 20:05:32 +0100

    Getters and setters, toString, equals and hashCode
    Added getters and setters methods, toString method, Equals method and hashCode method

--- src/main/java/no/ntnu/idatx2001/Link.java
+++ src/main/java/no/ntnu/idatx2001/Link.java
@@ -1,6 +1,7 @@
 package no.ntnu.idatx2001;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Link {
 
@@ -14,6 +15,13 @@ public class Link {
   // A list with special objects that makes it possible to affect the features of a player.
   private List<Action> actions;
 
+  /**
+   * Creates an instance of Link by taking in text and reference
+   *
+   * @param text a describing text that indicates a choice or an action in a story.
+   *             The text is the part of Link which is visible for the player.
+   * @param reference A string that identifies a passage (a part of a story).
+   */
   public Link(String text, String reference) {
     if (text.isBlank()) {
       IllegalArgumentException e = new IllegalArgumentException();
@@ -32,7 +40,66 @@ public class Link {
     }
   }
 
-  
+  /**
+   * Return the text of this link
+   * @return the text of this link
+   */
+  public String getText() {
+    return this.text;
+  }
 
+  /**
+   * Return the reference of this link
+   * @return the reference of this link
+   */
+  public String getReference() {
+    return this.reference;
+  }
 
+  /**
+   * Add an action to the action list
+   */
+  public void addAction(Action newAction) {
+    actions.add(newAction);
+  }
+
+  /**
+   * Return the list of actions
+   * @return the list of actions
+   */
+  public List<Action> getActions() {
+    return actions;
+  }
+
+  /**
+   * Return a formatted string, including all information about a link
+   * @return a formatted string, including all information about a link
+   */
+  @Override
+  public String toString() {
+    return "Link{" +
+        "text='" + text + '\'' +
+        ", reference='" + reference + '\'' +
+        ", actions=" + actions +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Link link = (Link) o;
+    return Objects.equals(text, link.text) &&
+        Objects.equals(reference, link.reference) &&
+        Objects.equals(actions, link.actions);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(text, reference, actions);
+  }
 }
