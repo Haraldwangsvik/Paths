d526b1b61c32c8456a9885e7ea62da5e23a89094
Harald <haraldwf@stud.ntnu.no>
2023-02-09 09:43:07 +0100

    LinkTest class
    Created LinkTest class

---
+++ src/test/java/no/ntnu/idatx2001/test/LinkTest.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatx2001.test;
+
+import no.ntnu.idatx2001.Link;
+import org.junit.Test;
+
+public class LinkTest {
+
+  @Test
+  public void testNullAsText() {
+    Link link = new Link(null, null);
+  }
+
+}
