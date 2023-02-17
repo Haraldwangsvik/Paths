dcc127dad19fa26675e0121303890f1651e725ec
Ertshus <siverer@stud.ntnu.no>
2023-02-17 14:50:00 +0100

    Made the interface Action, and added the execute method.

--- src/main/java/no/ntnu/idatx2001/InterfaceActions/Action.java
+++ src/main/java/no/ntnu/idatx2001/InterfaceActions/Action.java
@@ -1,4 +1,16 @@
 package no.ntnu.idatx2001.InterfaceActions;
 
-public class Action {
+import no.ntnu.idatx2001.Player;
+
+/**
+ * A class that represents future change in the state of the player.
+ * Includes changes to the player's score, health, gold or inventory.
+ */
+public interface Action {
+
+    /**
+     * Changes the state of the player when called upon.
+     * @param player The player that the state changes for
+     */
+    void execute(Player player);
 }
