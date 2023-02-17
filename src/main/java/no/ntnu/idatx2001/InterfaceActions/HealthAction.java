4694b12a7ce6e1cee20f6a1181cab550b3a0045e
Ertshus <siverer@stud.ntnu.no>
2023-02-17 14:52:39 +0100

    Created the class HealthAction and the execute method.

---
+++ src/main/java/no/ntnu/idatx2001/InterfaceActions/HealthAction.java
@@ -0,0 +1,22 @@
+package no.ntnu.idatx2001.InterfaceActions;
+
+import no.ntnu.idatx2001.Player;
+
+/**
+ * Represents a class that changes a players health.
+ */
+public class HealthAction {
+    private int health;
+
+    public HealthAction(int health) {
+        this.health = health;
+    }
+
+    /**
+     * Change the health of the player.
+     * @param player The player that the health changes for.
+     */
+    public void execute(Player player) {
+        player.addHealth(health);
+    }
+}
