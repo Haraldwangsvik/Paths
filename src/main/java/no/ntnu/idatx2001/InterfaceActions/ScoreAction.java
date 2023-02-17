39647190bd4f4ece26832cd9d4815cf163b5887b
Ertshus <siverer@stud.ntnu.no>
2023-02-17 14:53:05 +0100

    Created the class ScoreAction and the execute method.

---
+++ src/main/java/no/ntnu/idatx2001/InterfaceActions/ScoreAction.java
@@ -0,0 +1,22 @@
+package no.ntnu.idatx2001.InterfaceActions;
+
+import no.ntnu.idatx2001.Player;
+
+/**
+ * Represents a class that changes a players score.
+ */
+public class ScoreAction {
+    private int points;
+
+    public ScoreAction(int points) {
+        this.points = points;
+    }
+
+    /**
+     * Change the score of the player.
+     * @param player The player that the score changes for.
+     */
+    public void execute(Player player) {
+        player.addScore(points);
+    }
+}
