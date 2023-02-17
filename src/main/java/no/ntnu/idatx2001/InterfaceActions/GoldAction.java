3a09c8bd82b297effd6782074b3f60b6c3a74ead
Ertshus <siverer@stud.ntnu.no>
2023-02-17 14:52:22 +0100

    Created the class GoldAction and the execute method.

---
+++ src/main/java/no/ntnu/idatx2001/InterfaceActions/GoldAction.java
@@ -0,0 +1,22 @@
+package no.ntnu.idatx2001.InterfaceActions;
+
+import no.ntnu.idatx2001.Player;
+
+/**
+ * Represents a class that changes a players amount of gold.
+ */
+public class GoldAction {
+    private int gold;
+
+    public GoldAction(int gold) {
+        this.gold = gold;
+    }
+
+    /**
+     * Change the amount gold the player has.
+     * @param player The player that the gold changes for.
+     */
+    public void execute(Player player) {
+        player.addGold(gold);
+    }
+}
