2e073273a1273622a49a993c46aeefc05a443215
Harald <haraldwf@stud.ntnu.no>
2023-02-22 08:33:01 +0100

    Added isfulfilled method in goal

--- src/main/java/no/ntnu/idatx2001/goals/Goal.java
+++ src/main/java/no/ntnu/idatx2001/goals/Goal.java
@@ -1,5 +1,18 @@
 package no.ntnu.idatx2001.goals;
 
+import no.ntnu.idatx2001.Player;
+
+
 public class Goal {
-  
+
+  /**
+   * Check if goal is fulfilled
+   * @param player
+   * @return true on fulfilled goal, false on not fulfilled goal
+   */
+  public boolean isFulfilled(Player player) {
+    boolean isFulfilled = false;
+    //TODO - check if goals is fulfilled
+    return isFulfilled;
+  }
 }
