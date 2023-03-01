39bbf5a4a8d2ce8dfc764035f898b16c64530e82
Ertshus <siverer@stud.ntnu.no>
2023-03-01 19:45:05 +0100

    Added isFulfilled method in GoldGoal

---
+++ src/main/java/no/ntnu/idatx2001/goals/GoldGoal.java
@@ -0,0 +1,23 @@
+package no.ntnu.idatx2001.goals;
+
+import no.ntnu.idatx2001.Player;
+
+/**
+ * A class that represents the expected minimum gold.
+ */
+public class GoldGoal {
+    private final int minimumGold;
+
+    public GoldGoal(int minimumGold) {
+        this.minimumGold = minimumGold;
+    }
+
+    /**
+     * Returns true if the minimum gold is fulfilled and false if not.
+     * @param player The player whose gold is checked
+     * @return true if minimum gold is fulfilled and false if not
+     */
+    public boolean isFulfilled(Player player) {
+        return player.getGold() >= minimumGold;
+    }
+}
