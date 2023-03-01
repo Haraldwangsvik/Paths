e308ad85ae6bc8948068d10f7c1704d9b29dda4e
Ertshus <siverer@stud.ntnu.no>
2023-03-01 19:46:20 +0100

    Added isFulfilled method in ScoreGoal

---
+++ src/main/java/no/ntnu/idatx2001/goals/ScoreGoal.java
@@ -0,0 +1,23 @@
+package no.ntnu.idatx2001.goals;
+
+import no.ntnu.idatx2001.Player;
+
+/**
+ * A class that represents the expected minimum score.
+ */
+public class ScoreGoal {
+    private final int minimumPoints;
+
+    public ScoreGoal(int minimumPoints) {
+        this.minimumPoints = minimumPoints;
+    }
+
+    /**
+     * Returns true if the minimum points is fulfilled and false if not.
+     * @param player The player whose score is checked
+     * @return true if minimum points is fulfilled and false if not
+     */
+    public boolean isFulfilled(Player player) {
+        return player.getScore() >= minimumPoints;
+    }
+}
