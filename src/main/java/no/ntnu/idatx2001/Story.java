205c473cd2d1a2ec4101a9eb79bceca1ac13e5b2
Harald <haraldwf@stud.ntnu.no>
2023-03-05 11:43:34 +0100

    RemovePassages and getBrokenLinks

--- src/main/java/no/ntnu/idatx2001/Story.java
+++ src/main/java/no/ntnu/idatx2001/Story.java
@@ -1,6 +1,9 @@
 package no.ntnu.idatx2001;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -67,5 +70,37 @@ public class Story {
      */
     public Collection<Passage> getPassages() {
         return passages.values();
+    }
+
+    /**
+     * Removes a given passage from passages. The passage can't be removed if there is other passages linked to it.
+     *
+     * @param link to the passage to be removed
+     * @return true on passage removed, false if passage not removed
+     */
+    public boolean removePassage(Link link) {
+
+        boolean linksToOtherPassages = new HashSet<>(passages.values()).size() != passages.size();
+        if (linksToOtherPassages) {
+            passages.remove(link);
+        }
+        return linksToOtherPassages;
+    }
+
+    /**
+     * Find and return a list of links that refers to a passage not found in passages.
+     *
+     * @return list of dead links
+     */
+    public List<Link> getBrokenLinks() {
+        List<Link> deadLinks = new LinkedList<>();
+
+        for ( Link link : passages.keySet()) {
+            if(!passages.containsValue(link.getReference())) {
+                deadLinks.add(link);
+            }
+        }
+
+        return deadLinks;
     }
 }
