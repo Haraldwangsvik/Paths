50f461659d8a6479ae6cd69eef5901d13c27444d
Harald <haraldwf@stud.ntnu.no>
2023-03-17 11:53:59 +0100

    implement Action on the different action classes

--- src/main/java/no/ntnu/idatx2001/actions/GoldAction.java
+++ src/main/java/no/ntnu/idatx2001/actions/GoldAction.java
@@ -5,7 +5,7 @@ import no.ntnu.idatx2001.Player;
 /**
  * Represents a class that changes a players amount of gold.
  */
-public class GoldAction {
+public class GoldAction implements Action {
     private int gold;
 
     public GoldAction(int gold) {
